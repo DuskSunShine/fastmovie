b1a63ade78a7282ff220d9de18ef0cef16bb8995
Administrator <2049525995@qq.com>
2016-12-27 22:18:58 +0800

    改进1

--- app/src/main/java/com/scy/fastmovie/activity/MainActivity.java
+++ app/src/main/java/com/scy/fastmovie/activity/MainActivity.java
@@ -23,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup rgb_bottom;
     private RadioButton rb1;
     private FragmentManager fragmentManager;
+    private MovieFragment fragment_movie;
+    private CinemaFragment fragment_cinema;
+    private DiscoverFragment fragment_discover;
+    private MineFragment fragment_mine;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,10 +37,19 @@ public class MainActivity extends AppCompatActivity {
         }
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        FragmentTransaction fragment_movie = transaction.add(R.id.layout, new MovieFragment());
-        FragmentTransaction fragment_cinema = transaction.add(R.id.layout, new CinemaFragment());
-        FragmentTransaction fragment_discover = transaction.add(R.id.layout, new DiscoverFragment());
-        FragmentTransaction fragment_mine = transaction.add(R.id.layout, new MineFragment());
+        this.fragment_movie = new MovieFragment();
+        transaction.add(R.id.layout, this.fragment_movie);
+        this.fragment_cinema = new CinemaFragment();
+        transaction.add(R.id.layout, this.fragment_cinema);
+        this.fragment_discover = new DiscoverFragment();
+        transaction.add(R.id.layout, this.fragment_discover);
+        this.fragment_mine = new MineFragment();
+        transaction.add(R.id.layout, this.fragment_mine);
+        transaction.show(this.fragment_movie);
+        transaction.hide(this.fragment_cinema);
+        transaction.hide(this.fragment_discover);
+        transaction.hide(this.fragment_mine);
+        transaction.commit();
         initViews();
         setClickListener();
         useViews();
@@ -48,7 +61,45 @@ public class MainActivity extends AppCompatActivity {
         rgb_bottom.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-
+                for (int i = 0; i <rgb_bottom.getChildCount() ; i++) {
+                    RadioButton rb = (RadioButton) rgb_bottom.getChildAt(i);
+                    if (rb.isChecked()){
+                        switch (i){
+                            case 0:
+                                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                                transaction.show(fragment_movie);
+                                transaction.hide(fragment_mine);
+                                transaction.hide(fragment_discover);
+                                transaction.hide(fragment_cinema);
+                                transaction.commit();
+                                break;
+                            case 1:
+                                FragmentTransaction transaction2 = fragmentManager.beginTransaction();
+                                transaction2.hide(fragment_movie);
+                                transaction2.hide(fragment_mine);
+                                transaction2.hide(fragment_discover);
+                                transaction2.show(fragment_cinema);
+                                transaction2.commit();
+                                break;
+                            case 2:
+                                FragmentTransaction transaction3 = fragmentManager.beginTransaction();
+                                transaction3.hide(fragment_movie);
+                                transaction3.hide(fragment_mine);
+                                transaction3.show(fragment_discover);
+                                transaction3.hide(fragment_cinema);
+                                transaction3.commit();
+                                break;
+                            case 3:
+                                FragmentTransaction transaction4 = fragmentManager.beginTransaction();
+                                transaction4.hide(fragment_movie);
+                                transaction4.show(fragment_mine);
+                                transaction4.hide(fragment_discover);
+                                transaction4.hide(fragment_cinema);
+                                transaction4.commit();
+                                break;
+                        }
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/scy/fastmovie/fragment/MovieFragment.java
+++ app/src/main/java/com/scy/fastmovie/fragment/MovieFragment.java
@@ -1,11 +1,17 @@
 package com.scy.fastmovie.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.scy.fastmovie.R;
 
@@ -14,6 +20,14 @@ import com.scy.fastmovie.R;
  */
 public class MovieFragment extends Fragment {
 
+
+    private View view;
+    private Toolbar toolbar;
+    private RadioGroup rgb;
+    private RadioButton rb_hot;
+    private RadioButton rb_wait;
+    private RadioButton rb_seek;
+    private ViewPager pager;
 
     public MovieFragment() {
         // Required empty public constructor
@@ -24,7 +38,73 @@ public class MovieFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        view = inflater.inflate(R.layout.fragment_blank, container, false);
+        initViews();
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setClickListener();
+        rb_hot.setChecked(true);
+        rb_hot.setBackgroundResource(R.drawable.top_checked_shape);
+        rb_wait.setBackgroundResource(R.drawable.top_unchecked_shape);
+        rb_seek.setBackgroundResource(R.drawable.top_unchecked_shape);
+        return view;
+    }
+
+    private void setClickListener() {
+        rgb.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.hot:
+                        rb_hot.setBackgroundResource(R.drawable.top_checked_shape);
+                        rb_wait.setBackgroundResource(R.drawable.top_unchecked_shape);
+                        rb_seek.setBackgroundResource(R.drawable.top_unchecked_shape);
+                        break;
+                    case R.id.wait:
+                        rb_wait.setBackgroundResource(R.drawable.top_checked_shape);
+                        rb_hot.setBackgroundResource(R.drawable.top_unchecked_shape);
+                        rb_seek.setBackgroundResource(R.drawable.top_unchecked_shape);
+                        break;
+                    case R.id.seek:
+                        rb_seek.setBackgroundResource(R.drawable.top_checked_shape);
+                        rb_wait.setBackgroundResource(R.drawable.top_unchecked_shape);
+                        rb_hot.setBackgroundResource(R.drawable.top_unchecked_shape);
+                        break;
+                }
+                for (int i = 0; i <rgb.getChildCount() ; i++) {
+                    RadioButton rb = (RadioButton) rgb.getChildAt(i);
+                    if (rb.isChecked()){
+                        pager.setCurrentItem(i);
+                    }
+                }
+            }
+        });
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                RadioButton rb = (RadioButton) rgb.getChildAt(position);
+                rb.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void initViews() {
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        rgb = ((RadioGroup) view.findViewById(R.id.rgb));
+        rb_hot = ((RadioButton) view.findViewById(R.id.hot));
+        rb_wait = ((RadioButton) view.findViewById(R.id.wait));
+        rb_seek = ((RadioButton) view.findViewById(R.id.seek));
+        pager = ((ViewPager) view.findViewById(R.id.pager));
     }
 
 }
