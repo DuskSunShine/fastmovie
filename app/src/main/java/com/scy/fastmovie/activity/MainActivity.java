25aea554a9b0bf02d71acb31625be723352f8c9c
Administrator <2049525995@qq.com>
2016-12-27 18:49:26 +0800

    改进

--- app/src/main/java/com/scy/fastmovie/activity/MainActivity.java
+++ app/src/main/java/com/scy/fastmovie/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,15 +13,16 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.scy.fastmovie.R;
+import com.scy.fastmovie.fragment.CinemaFragment;
+import com.scy.fastmovie.fragment.DiscoverFragment;
+import com.scy.fastmovie.fragment.MineFragment;
+import com.scy.fastmovie.fragment.MovieFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private RadioGroup rgb;
-    private RadioButton rb_hot;
-    private RadioButton rb_wait;
-    private RadioButton rb_seek;
-    private ViewPager pager;
+    private RadioGroup rgb_bottom;
+    private RadioButton rb1;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,30 +31,34 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT>=21){
             getWindow().setStatusBarColor(Color.argb(200,63,81,181));
         }
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        FragmentTransaction fragment_movie = transaction.add(R.id.layout, new MovieFragment());
+        FragmentTransaction fragment_cinema = transaction.add(R.id.layout, new CinemaFragment());
+        FragmentTransaction fragment_discover = transaction.add(R.id.layout, new DiscoverFragment());
+        FragmentTransaction fragment_mine = transaction.add(R.id.layout, new MineFragment());
         initViews();
         setClickListener();
         useViews();
+
     }
 
     private void setClickListener() {
-        rgb.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+        rgb_bottom.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                // TODO: 2016/12/27
+
             }
         });
     }
 
     private void useViews() {
-        setSupportActionBar(toolbar);
+        rb1.setChecked(true);
     }
 
     private void initViews() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        rgb = (RadioGroup) findViewById(R.id.rgb);
-        rb_hot = (RadioButton) findViewById(R.id.hot);
-        rb_wait = (RadioButton) findViewById(R.id.wait);
-        rb_seek = (RadioButton) findViewById(R.id.seek);
-        pager = (ViewPager) findViewById(R.id.pager);
+        rgb_bottom = (RadioGroup) findViewById(R.id.rgb_bottom);
+        rb1 = (RadioButton) findViewById(R.id.rb_1);
     }
 }
