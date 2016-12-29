853d3bac9c20500653928a168aeeed788650f284
Administrator <1029817776@qq.com>
2016-12-29 10:57:21 +0800

    111

--- app/src/main/java/com/scy/fastmovie/fragment/MineFragment.java
+++ app/src/main/java/com/scy/fastmovie/fragment/MineFragment.java
@@ -15,16 +15,12 @@ import com.scy.fastmovie.R;
 public class MineFragment extends Fragment {
 
 
-    public MineFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view=inflater.inflate(R.layout.fragment_mine,container,false);
+        return view;
     }
 
 }
