c73976be68acde8cc52c00277fdaf8a695c85b30
Administrator <437433575@qq.com>
2016-12-29 20:23:17 +0800

    点击事件

--- app/src/main/java/com/scy/fastmovie/httpapiservice/HttpApiService.java
+++ app/src/main/java/com/scy/fastmovie/httpapiservice/HttpApiService.java
@@ -1,9 +1,21 @@
 package com.scy.fastmovie.httpapiservice;
 
+import com.scy.fastmovie.bean.DiscoverBean;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
 /**
  * Created by SCY on 2016/12/29 9:27.
  */
 
 public interface HttpApiService {
+    //发现列表
+    @GET("sns/v5/feed.json?")
+    Observable<DiscoverBean> getDiscoverData(
+            @Query("offset") String offset,
+            @Query("limit") String limit
+    );
     
 }
